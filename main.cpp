c802b2209a3e6548ff735bc7cb68c0d499422ed6
Kyrylo <vetrovweb@icloud.com>
2026-05-29 19:08:53 +0300

    Add new lessons

--- main.cpp
+++ main.cpp
@@ -5,29 +5,48 @@ using namespace std;
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
+    // // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
+    //
+    // const auto lang = "C++";
+    // //std::cout << "Hello and welcome to " << lang << "!\n";
+    // std::cout << "Hello, world!\n";
+    // std::cout << 5,3;
+    // std::cout << "\t" << 5.4;
+    // std::cout << "\n" << 2 * 5,3; // can't see after ","
+    //
+    // int a = 5;
+    // float b = 5;
+    // std::cout << "\n" << a / 7;
+    // std::cout << "\n" << b / 7;
+    //
+    // // out size variable
+    // long int c = 19234834127283;
+    // short int d = 32;
+    // int f;
+    //
+    // cout << "\n" << sizeof(d) << endl;
+    // // cout << "\n" << "Введіть змінну f" << endl;
+    // // cin >> f;
+    // // cout << f << endl;1
+    //
+    //
+    // // lessons 37 (3-тє заняття)
+    // bool t1 = 12;
+    // std::cout << t1 << endl;
 
-    const auto lang = "C++";
-    //std::cout << "Hello and welcome to " << lang << "!\n";
-    std::cout << "Hello, world!\n";
-    std::cout << 5,3;
-    std::cout << "\t" << 5.4;
-    std::cout << "\n" << 2 * 5,3; // can't see after ","
+    int age{};
+    std::cout << "Введіть Ваш вік: ";
+    std::cin >> age;
 
-    int a = 5;
-    float b = 5;
-    std::cout << "\n" << a / 7;
-    std::cout << "\n" << b / 7;
-
-    // out size variable
-    long int c = 19234834127283;
-    short int d = 32;
-    int f;
-
-    cout << "\n" << sizeof(d) << endl;
-    // cout << "\n" << "Введіть змінну f" << endl;
-    // cin >> f;
-    // cout << f << endl;1
+    if (age < 18) {
+        std::cout << "Вам менше 18-ти. Упс..." << std::endl;
+    }
+    else if (age >= 30 ){
+        std::cout << "Трохи старий, але красавчик" << std::endl;
+    }
+    else {
+        std::cout << "Ви красавчик" << std::endl;
+    }
 
     return 0;
 }
--- task6.cpp
+++ task6.cpp
@@ -6,11 +6,12 @@
 
 int main() {
 
-
     short x = 32767;
 
     std::cout << "short x = " << x << std::endl;
     x = x + 1;
     std::cout << "x = x + 1 --- " << x << std::endl;
+    //std::cout << std::numeric_limits<short>::min() << std::endl;
+
     return 0;
 }
