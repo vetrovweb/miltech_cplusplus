40cd7c4c8814950fdee95738409bc0375dbb6228
Kyrylo <vetrovweb@icloud.com>
2026-08-11 11:59:41 +0300

    Add coins and exit

--- games/maze.cpp
+++ games/maze.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-void movePlayer(vector<vector<char>>& new_field, int& x, int& y, int& step, char move) {
+void movePlayer(vector<vector<char>>& new_field, int& x, int& y, int& step, char move, int& coins) {
     // Питаємо що попереду і якщо можна, заміняємо місцями символи
     switch (move) {
         case 'w':
@@ -22,6 +22,20 @@ void movePlayer(vector<vector<char>>& new_field, int& x, int& y, int& step, char
             else if (new_field[x-1][y] == '#') {
                 step++;
             }
+            else if (new_field[x-1][y] == '$') {
+                new_field[x][y] = '.';
+                x--;
+                new_field[x][y] = 'P';
+                step++;
+                coins++;
+            }
+            else if (new_field[x-1][y] == 'E') {
+                new_field[x-1][y] = '.';
+                x--;
+                new_field[x][y] = 'P';
+                step++;
+                cout << "You Win! Congratulations!" << endl;
+            }
         break;
 
         case 'a':
@@ -33,6 +47,20 @@ void movePlayer(vector<vector<char>>& new_field, int& x, int& y, int& step, char
             }
             else if (new_field[x][y-1] == '#') {
                 step++;
+            }
+            else if (new_field[x][y-1] == '$') {
+                new_field[x][y] = '.';
+                y--;
+                new_field[x][y] = 'P';
+                step++;
+                coins++;
+            }
+            else if (new_field[x][y-1] == 'E') {
+                new_field[x][y-1] = '.';
+                y--;
+                new_field[x][y] = 'P';
+                step++;
+                cout << "You Win! Congratulations!" << endl;
             }
         break;
 
@@ -46,6 +74,22 @@ void movePlayer(vector<vector<char>>& new_field, int& x, int& y, int& step, char
             else if (new_field[x+1][y] == '#') {
                 step++;
             }
+            else if (new_field[x+1][y] == '$') {
+                new_field[x][y] = '.';
+                x++;
+                new_field[x][y] = 'P';
+                // Постійно оновлюємо (не залежно від напрямку)
+                step++;
+                coins++;
+            }
+            else if (new_field[x+1][y] == 'E') {
+                new_field[x][y] = '.';
+                x++;
+                new_field[x][y] = 'P';
+                // Постійно оновлюємо (не залежно від напрямку)
+                step++;
+                cout << "You Win! Congratulations!" << endl;
+            }
         break;
 
         case 'd':
@@ -57,6 +101,22 @@ void movePlayer(vector<vector<char>>& new_field, int& x, int& y, int& step, char
             }
             else if (new_field[x][y+1] == '#') {
                 step++;
+            }
+            else if (new_field[x][y+1] == '$') {
+                new_field[x][y] = '.';
+                y++;
+                new_field[x][y] = 'P';
+                // Постійно оновлюємо (не залежно від напрямку)
+                step++;
+                coins++;
+            }
+            else if (new_field[x][y+1] == 'E') {
+                new_field[x][y] = '.';
+                x++;
+                new_field[x][y] = 'P';
+                // Постійно оновлюємо (не залежно від напрямку)
+                step++;
+                cout << "You Win! Congratulations!" << endl;
             }
         break;
 
@@ -97,6 +157,7 @@ int main() {
     int step = 0;                                                                   // рахунок ходів (кроків)
     int x = 1, y = 1;                                                               // початкові координати гравця
     char control;                                                                   // кнопка
+    int coins = 0;                                                                  // монети
 
     // ----------------------------------------------------------------------------
     // Playing field
@@ -129,30 +190,34 @@ int main() {
 
             // up
             case 'w':
-                movePlayer(playingField, x, y, step, control);
+                movePlayer(playingField, x, y, step, control, coins);
                 Show(playingField);
                 cout << "Count steps: " << step << endl;
+                cout << "Coins: " << coins << endl;
                 break;
 
             // left
             case 'a':
-                movePlayer(playingField, x, y, step, control);
+                movePlayer(playingField, x, y, step, control, coins);
                 Show(playingField);
                 cout << "Count steps: " << step << endl;
+                cout << "Coins: " << coins << endl;
                 break;
 
             // down
             case 's':
-                movePlayer(playingField, x, y, step, control);
+                movePlayer(playingField, x, y, step, control, coins);
                 Show(playingField);
                 cout << "Count steps: " << step << endl;
+                cout << "Coins: " << coins << endl;
                 break;
 
             // right
             case 'd':
-                movePlayer(playingField, x, y, step, control);
+                movePlayer(playingField, x, y, step, control, coins);
                 Show(playingField);
                 cout << "Count steps: " << step << endl;
+                cout << "Coins: " << coins << endl;
                 break;
 
             //case 9: return 0;
